8e5f4d259d67c28b1d3a0918b67e7d18fb2a3561
mutluerengazi <erengazi.mutlu@ug.bilkent.edu.tr>
2020-07-11 03:40:33 +0300

    new things

---
+++ javascript/week4/demoXYZ/javascript1/testing.js
@@ -0,0 +1,18 @@
+var customerName = "Eren";
+var customerSurname = "Mutlu";
+var id = "123456789";
+var Total = 300;
+var gender = "erkek";
+var adress = {
+    city: "ankara",
+    district: "bilkent",
+    more: "merkez kampus",
+}
+var hobbies = ["books","basketball","blabla"];
+
+let sampleDate = new Date();
+console.log(sampleDate);
+
+val = Math.floor(Math.random()*100);
+console.log(val);
+
